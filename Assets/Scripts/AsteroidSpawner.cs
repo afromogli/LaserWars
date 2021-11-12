f100d1db3b315fb9c7dd35f17eb07cb85472a234
Aram Taieb <aram.paul.taieb@gmail.com>
2021-11-12 14:23:09 +0100

    -Added asteroid spawner

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject AsteroidPrefab { get; set; }
+
+    public float Density { get; set; }
+
+    public Vector3 WorldCenterPoint { get; set; }
+
+    public float CubeSideLength { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float LerpSpeed = 10;
+    //public float LerpSpeed = 10;
     public Transform TargetObject;
 
     private Vector3 cameraOffset;
