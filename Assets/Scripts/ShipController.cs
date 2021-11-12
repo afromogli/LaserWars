add13a899345a9a237d65be7fbb8ae136f78f7e9
Aram Taieb <aram.paul.taieb@gmail.com>
2021-11-12 13:56:48 +0100

    Tweaked ship speed

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,8 +7,8 @@ public class ShipController : MonoBehaviour
     // ForwardSpeed = forwards/backwards
     // StrafeSpeed = left/right
     // HoverSpeed = up/down
-    public float ForwardSpeed = 25f, StrafeSpeed = 25f, HoverSpeed = 5f;
-    public float ForwardAcceleration = 5f, StrafeAcceleration = 2.5f, HoverAcceleration = 2.5f;
+    public float ForwardSpeed = 15f, StrafeSpeed = 5f, HoverSpeed = 5f;
+    public float ForwardAcceleration = 2.5f, StrafeAcceleration = 2.5f, HoverAcceleration = 2.5f;
 
     private float currentForwardSpeed, currentStrafeSpeed, currentHoverSpeed;
 
@@ -25,10 +25,10 @@ public class ShipController : MonoBehaviour
         currentStrafeSpeed = Mathf.Lerp(currentStrafeSpeed, Input.GetAxis("Horizontal") * StrafeSpeed, StrafeAcceleration * Time.deltaTime);
         currentHoverSpeed = Mathf.Lerp(currentHoverSpeed, Input.GetAxis("Hover") * HoverSpeed, HoverAcceleration * Time.deltaTime);
 
-        currentForwardSpeed = Mathf.Min(currentForwardSpeed, ForwardSpeed);
-        currentStrafeSpeed = Mathf.Min(currentStrafeSpeed, StrafeSpeed);
-        currentHoverSpeed = Mathf.Min(currentHoverSpeed, HoverSpeed);
+        //currentForwardSpeed = Mathf.Min(currentForwardSpeed, ForwardSpeed * Time.deltaTime);
+        //currentStrafeSpeed = Mathf.Min(currentStrafeSpeed, StrafeSpeed * Time.deltaTime);
+        //currentHoverSpeed = Mathf.Min(currentHoverSpeed, HoverSpeed * Time.deltaTime);
 
-        transform.position += (currentForwardSpeed * transform.forward) + (currentStrafeSpeed * transform.right) + (currentHoverSpeed * transform.up);
+        transform.position += (currentForwardSpeed * transform.forward) + (currentStrafeSpeed * transform.right.normalized) + (currentHoverSpeed * transform.up.normalized);
     }
 }
