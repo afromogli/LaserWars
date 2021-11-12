0abee2b09cf23783203bcc1b41fa556f02b82f22
Aram Taieb <aram.paul.taieb@gmail.com>
2021-11-12 15:56:32 +0100

    -Continued working on Asteroid spawner

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -1,26 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
+using Assets.Scripts.Common;
 using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-    public GameObject AsteroidPrefab { get; set; }
+    public GameObject AsteroidPrefab;
 
-    public float Density { get; set; }
+    public float Spacing;
 
-    public Vector3 WorldCenterPoint { get; set; }
+    public float RandomSpacing;
 
-    public float CubeSideLength { get; set; }
+    public Vector3 WorldCenterPoint;
+
+    //public float CubeSideLength;
+
+    //public int AsteroidCount;
+
+    public int ObjectsPerSide;
+
+    private ObjectPool objectPool;
+
+    //private GameObject[] activeAsteroids;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        int objectCount = ObjectsPerSide * ObjectsPerSide * ObjectsPerSide;
+        Debug.Log("objectCount: " + objectCount);
+        objectPool = new ObjectPool(objectCount, () => { return Instantiate<GameObject>(AsteroidPrefab); });
+        //activeAsteroids = new GameObject[AsteroidCount];
+
+        Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    private void Spawn()
+    {
+        /////////////////////////////////////////////////
+        // Spawns asteroids inside a cubic volume
+        /////////////////////////////////////////////////
+
+        // Total distance = Spacing * AsteroidCount / NumberOfLayers
+        //int asteroidCountPerLayer = AsteroidCount / CubeSideSize;
+        float sideLengthPerLayer = Spacing * ObjectsPerSide;
+        float halfSideLength = sideLengthPerLayer / 2;
+
+        Debug.Log("sideLengthPerLayer: " + sideLengthPerLayer);
+        Debug.Log("halfSideLength: " + halfSideLength);
+        Debug.Log("ObjectsPerSide: " + ObjectsPerSide);
+
+        Vector3 position = Vector3.zero;
+        // Bottom layer to top layer
+        for (int y = 0; y < ObjectsPerSide; y++)
+        {
+            for (int x = 0; x < ObjectsPerSide; x++)
+            {
+                for (int z = 0; z < ObjectsPerSide; z++)
+                {
+                    GameObject gameObject = objectPool.GetObjectFromPool();
+                    gameObject.SetActive(true);
+                    position.x = (WorldCenterPoint.x - halfSideLength) + x * Spacing + (Random.value * RandomSpacing);
+                    position.y = (WorldCenterPoint.y - halfSideLength) + y * Spacing + (Random.value * RandomSpacing);
+                    position.z = (WorldCenterPoint.z - halfSideLength) + x * Spacing + (Random.value * RandomSpacing);
+                    gameObject.transform.position = position;
+
+                    Random.InitState(System.DateTime.Now.Millisecond);
+                }                    
+                // TODO: add random rotation?
+            }
+        }
+
     }
 }
---
+++ Assets/Scripts/Common/ObjectPool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Common
+{
+    public class ObjectPool
+    {
+        private GameObject[] pooledObjects;
+
+        public ObjectPool(int capacity, Func<GameObject> createGameObjectMethod)
+        {
+            pooledObjects = new GameObject[capacity];
+
+            GameObject newObject;
+            for (int i = 0; i < capacity; i++)
+            {
+                newObject = createGameObjectMethod();
+                newObject.SetActive(false);
+                pooledObjects[i] = newObject;
+            }
+        }
+
+        public GameObject GetObjectFromPool()
+        {
+            for (int i = 0; i < pooledObjects.Length; i++)
+            {
+                if (!pooledObjects[i].activeInHierarchy)
+                {
+                    return pooledObjects[i];
+                }
+            }
+            return null;
+        }
+    }
+}
