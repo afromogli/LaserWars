96458122c7ced2870cddc4e1edab94f9ea5c56ac
ata <aram.taieb@combination.se>
2021-12-10 11:47:56 +0100

    Started working on collisions between pulse projectiles and asteroids

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Asteroid : MonoBehaviour
+    {
+        public Transform ExplosionPrefab;
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "PulseProjectile")
+            {
+                ContactPoint contact = collision.contacts[0];
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+                Vector3 position = contact.point;
+
+                Debug.Log("Asteroid destroyed");
+                //Instantiate(ExplosionPrefab, position, rotation);
+                Destroy(gameObject);
+            }           
+        }
+    }
+}
